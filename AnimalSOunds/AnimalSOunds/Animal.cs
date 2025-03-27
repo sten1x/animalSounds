53388ecb8bd7ec72c7ac45dc930ae5fecdc0f3ae
sten1x <115377465+sten1x@users.noreply.github.com>
2025-03-27 17:39:24 +0200

    Add files via upload

---
+++ AnimalSOunds/AnimalSOunds/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSOunds
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; }
+        public abstract string MakeSound();
+    }
+}
---
+++ AnimalSOunds/AnimalSOunds/Cow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSOunds
+{
+    public class Cow : Animal
+    {
+        public override string Name => "Cow";
+        public override string MakeSound() => "Moo Moo";
+    }
+}
