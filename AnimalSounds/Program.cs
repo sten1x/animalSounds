159b2fc99f3ce948db10db4c994d65c2eabc7782
Steavu Nicolae <115377465+sten1x@users.noreply.github.com>
2025-04-27 22:15:49 +0300

    changed files based on review

---
+++ AnimalSounds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Dog(),
+                new Cat(),
+                new Cow()
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.MakeSound()}");
+            }
+        }
+    }
+}
